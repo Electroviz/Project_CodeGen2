2d284193479fb0eac1f7120c1b35fe40894f66ef
DatIsHetLeven <muratcelem187@gmail.com>
2022-06-05 14:52:34 +0200

    Update TransactionService.java
    Added list of things to implement for making a transaction

--- src/main/java/io/swagger/service/TransactionService.java
+++ src/main/java/io/swagger/service/TransactionService.java
@@ -24,21 +24,42 @@ public class TransactionService {
         //Check if amount is valid
         if (transaction.getAmount().doubleValue() <=0)
             throw ApiException.badRequest("Invalid amount");
+
         //Check the iban from the sender + receiver
         BankAccount fromBankAccount = bankAccountService.GetBankAccountByIban(transaction.getFrom());
         BankAccount toBankAccount = bankAccountService.GetBankAccountByIban(transaction.getTo());
+
         //Prevent transfers from a savings account to an account of not the same customer
         if(fromBankAccount.getAccountType().equals(BankAccount.AccountTypeEnum.SAVINGS)){
             if (!Objects.equals(fromBankAccount.getUserId(), toBankAccount.getUserId())){
                 throw ApiException.badRequest("Transactions from a savings account should be to an account of the same customer");
             }
         }
+
         //Prevent transfers from an account to a savings account of not the same customer
         if (toBankAccount.getAccountType().equals(BankAccount.AccountTypeEnum.SAVINGS)) {
             if (!Objects.equals(fromBankAccount.getUserId(), toBankAccount.getUserId())) {
                 throw ApiException.badRequest("Transactions to a savings account should be to an account of the same customer");
             }
         }
+
+        //Get the from user (the sender)
+        //Someone needs to implement userservice???
+
+
+
+
+        //Things to implement!!!!
+        //Get the from user (the sender)
+        //Check transaction limit of the user
+        //Get the sender and receiver bank iban
+        //Check the new balance is not less than the absolut limit
+        //The amount of the transaction should not exceed the day limit
+        //Compute the total day transactions if this transaction were to go through
+
+        //Update the senders bank account
+        //UPdate the receivers bank account
+        //Save the transaction
 
 
     }
