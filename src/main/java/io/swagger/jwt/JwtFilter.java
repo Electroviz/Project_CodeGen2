4166b7803ac6ade468f740f92f3fa63db740fc04
Nicky Garcia Jorge <619307@student.inholland.nl>
2022-06-07 20:55:16 +0200

    bezig geweest met jwt

---
+++ src/main/java/io/swagger/jwt/JwtFilter.java
@@ -0,0 +1,13 @@
+package io.swagger.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class JwtFilter {
+    @Autowired
+    private JwtProvider jwtTokenProvider;
+
+    @Override
+    private void FilterJwt(){
+
+    }
+}
--- src/main/java/io/swagger/jwt/JwtProvider.java
+++ src/main/java/io/swagger/jwt/JwtProvider.java
@@ -1,10 +1,16 @@
 package io.swagger.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.swagger.model.User;
+import io.swagger.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.PostConstruct;
 import java.util.Base64;
@@ -14,8 +20,15 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class JwtProvider {
+    @Value("${security.jwt.token.secret-key:secret-key}")
     private String secretToken;
+
+    @Value("${security.jwt.token.expire-length:36000000}")
     private int validityTime = 36000000; //10h in milliseconds
+
+    @Autowired
+    private UserService thisUserService;
+
 
     @PostConstruct
     protected void Initiate()
@@ -36,5 +49,25 @@ public class JwtProvider {
                 .setExpiration(validity)//
                 .signWith(SignatureAlgorithm.HS256, secretToken)//
                 .compact();
+    }
+
+    public void GetAuthentication(){
+
+    }
+
+    public String GetUserName(String jwtToken){
+        UserService userService = thisUserService.loadUser(GetUserName(jwtToken))
+    }
+
+    public void ResolveToken(){
+
+    }
+    public boolean ValidateToken(String jwtToken){
+        try {
+            Jwts.parser().setSigningKey(secretToken).parseClaimsJws(jwtToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "JWT token is invalid or expired");
+        }
     }
 }
