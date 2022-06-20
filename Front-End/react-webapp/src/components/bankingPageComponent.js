ab0372b10bff2aabcccf6a2bdfc9a2bc6bb6e5a2
mrjoker <makkertje330@live.nl>
2022-06-20 20:11:36 +0200

    Create a bank account for a user

--- Front-End/react-webapp/src/components/bankingPageComponent.js
+++ Front-End/react-webapp/src/components/bankingPageComponent.js
@@ -149,7 +149,11 @@ const bankingPageComponent = () => {
                             document.getElementById("currentUserFullName").innerHTML = "Welcome " + res.data["fullname"];
                             userRights = res.data["role"];
                             if(userRights != "employee") document.getElementById("EmployeeContainer").remove();
-                            else loadAllBankAccountInfo();
+                            else {
+                                //load all info for the employee
+                                loadAllUsersWithoutBankAccounts();
+                                loadAllBankAccountInfo();
+                            }
                         }
                     });
                 }
@@ -185,25 +189,62 @@ const bankingPageComponent = () => {
                 });
     }
 
-    // const ChangeBankAccountStatus = (e) => { 
-    //     var iban = document.getElementById("changeStatusIban").value;
-    //     var status = document.getElementById("changeStatus").value;
-    //     console.log(iban + "  and   " + status);
+    const EmployeeCreateBankAccountForUser = (e) => {
+        // /initBankAccounts/{userId}
+        instance.post('http://localhost:8080/api/initBankAccounts/' + document.getElementById("inputCreateBankAccUserId").value, {
+                headers: {
+                    'Content-Type': null,
+                    Authorization: "Bearer " + jwtToken,
+                }
+                })
+                .then(res => { 
+                    if(res.status >= 200 && res.status <= 300) {
+                        alert("Succes");
+                        loadAllUsersWithoutBankAccounts();
+                    }
+                }).catch((error) => {
+                    alert("Failed to change the status");
+                });
+    }
 
-    //     instance.get('http://localhost:8080/api/user/get/' + userId, {
-    //                 headers: {
-    //                     'Content-Type': null,
-    //                     Authorization: "Bearer " + jwtToken,
-    //                 }
-    //                 })
-    //                 .then(res => { 
-    //                     if(res.status >= 200 && res.status <= 300) {
-    //                         alert("Succesfully changed");
-    //                         window.location.reload;
-    //                     }
-    //                 });
-            
-    // }
+    function loadAllUsersWithoutBankAccounts() {
+        instance.get('http://localhost:8080/api/user/getAllUsersWithoutBankAccounts', {
+            headers: {
+                'Content-Type': null,
+                Authorization: "Bearer " + jwtToken,
+            }
+            })
+            .then(res => { 
+                if(res.status >= 200 && res.status <= 300) {
+                    var container = document.getElementById("allUsersWithoutBankAccountsList");
+                    container.innerHTML = "";
+                    if(res.data.length > 0) {
+                        for(let i = 0; i < res.data.length; i++) {
+                            var userAccInfoElm = document.createElement("p");
+                            userAccInfoElm.style.textAlign = "center";
+                            userAccInfoElm.style.marginBottom = "0.8rem";
+                            userAccInfoElm.style.borderBottom = "0.1rem solid black";
+                            userAccInfoElm.innerHTML = 
+                            "Id: " + res.data[i]["id"] + ", " +
+                            "Fullname: " + res.data[i]["fullname"] + ", " +
+                            "Email: " + res.data[i]["email"] + ", " +
+                            "Phone: " + res.data[i]["phone"] + ", " +
+                            "Role: " + res.data[i]["role"];
+
+                            container.append(userAccInfoElm);
+                        }
+                    }
+                    else {
+                        var emptyTextElm = document.createElement("p");
+                        emptyTextElm.innerHTML = "No users exists without a bank account";
+                        container.append(emptyTextElm);
+                    }
+                }
+                else {
+                    
+                }
+            });
+    }
 
     function loadAllBankAccountInfo() {
         instance.get('http://localhost:8080/api/allBankAccounts', {
@@ -233,7 +274,6 @@ const bankingPageComponent = () => {
                         
                         
                         container.append(bankAccInfoElm);
-                        
                     }
                 }
             });
@@ -241,7 +281,7 @@ const bankingPageComponent = () => {
     
     return (
         <div>
-            <div style={{ marginTop: "0.8rem", width: "100%", paddingLeft: "10rem;", borderBottom: "0.4rem solid black"}}>
+            <div style={{ marginTop: "0.8rem", width: "100%", borderBottom: "0.4rem solid black"}}>
                 <h1>BANKING APPLICATION</h1>
                 <div style={{ display: "block" }}>
                     <p id="currentUserFullName" style={{ width: "100%", textAlign: "center"}}></p>
@@ -298,7 +338,7 @@ const bankingPageComponent = () => {
                 <h4 style={{ fontSize: "3rem", borderTop: "0.4rem solid black"}}>Employee</h4>
 
                 <div>
-                    <p style={{ fontSize: "1.4rem", fontWeight: "bold", marginBottom: "0rem"}}>All bank accounts info</p>
+                    <p style={{ fontSize: "1.4rem", fontWeight: "bold", marginBottom: "0rem"}}>All bank accounts info:</p>
                     <div id="allBankAccountsList" style={{height: "20rem", width: "50%", marginLeft: "25%", overflow: "auto", display: "block"}}>
                         
                     </div>
@@ -311,6 +351,17 @@ const bankingPageComponent = () => {
                         <option>Closed</option>
                     </select>
                     <button onClick={ ChangeBankAccountStatus } style={{ display: "inline-block", fontSize: "1.15rem", marginLeft: "0.2rem"}}>Confirm</button>
+
+                </div>
+
+                <div>
+                    <p style={{ fontSize: "1.4rem", fontWeight: "bold", marginBottom: "0rem"}}>All users without bankaccount:</p>
+                    <div id="allUsersWithoutBankAccountsList" style={{height: "auto", width: "50%", marginLeft: "25%", overflow: "auto", display: "block"}}>
+                        
+                    </div>
+                    <p style={{ fontSize: "1.15rem", marginBottom: "0rem", display: "inline-block"}}>Create bank accounts for user id: </p>
+                    <input id="inputCreateBankAccUserId" defaultValue="0" style={{ fontSize: "1.15rem", marginBottom: "0rem", display: "inline-block", marginLeft: "0.2rem", marginRight: "0.2rem", width: "5rem", textAlign: "center" }} placeholder="user id" />
+                    <button onClick={ EmployeeCreateBankAccountForUser } style={{ display: "inline-block", fontSize: "1.15rem", marginLeft: "0.2rem"}}>Confirm</button>
 
                 </div>
             </div>
