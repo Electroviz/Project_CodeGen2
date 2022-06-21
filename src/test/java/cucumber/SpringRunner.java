9801d09a39dfeb5690d4ef8b467b5bed4d526931
Nicky Garcia Jorge <619307@student.inholland.nl>
2022-06-21 21:31:18 +0200

    test

--- src/test/java/cucumber/SpringRunner.java
+++ src/test/java/cucumber/SpringRunner.java
@@ -4,7 +4,5 @@ import org.junit.internal.Classes;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(org.springframework.test.context.junit4.SpringRunner.class)
-@SpringBootTest(Classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringRunner {
 }
