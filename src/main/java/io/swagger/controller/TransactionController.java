7bbdb1f1af00a494adc09459055e48f74ebcc785
DatIsHetLeven <muratcelem187@gmail.com>
2022-06-21 17:46:04 +0200

    ..

--- src/main/java/io/swagger/controller/TransactionController.java
+++ src/main/java/io/swagger/controller/TransactionController.java
@@ -1,41 +1,36 @@
 package io.swagger.controller;
 
-import io.swagger.api.ApiException;
-import io.swagger.api.TransactionsApiController;
-import io.swagger.jwt.JwtTokenProvider;
-import io.swagger.model.ApiResponse;
-import io.swagger.model.TransactionRequest;
-import io.swagger.model.UserDTO;
-import io.swagger.model.entity.User;
-import io.swagger.service.TransactionService;
-import io.swagger.service.UserService;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+        import io.swagger.api.ApiException;
+        import io.swagger.api.TransactionsApiController;
+        import io.swagger.jwt.JwtTokenProvider;
+        import io.swagger.model.ApiResponse;
+        import io.swagger.model.BankAccount;
+        import io.swagger.model.TransactionRequest;
+        import io.swagger.model.UserDTO;
+        import io.swagger.model.entity.User;
+        import io.swagger.service.TransactionService;
+        import io.swagger.service.UserService;
+        import io.swagger.v3.oas.annotations.Parameter;
+        import io.swagger.v3.oas.annotations.enums.ParameterIn;
+        import io.swagger.v3.oas.annotations.media.Schema;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.MediaType;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.security.core.Authentication;
+        import org.springframework.security.core.context.SecurityContextHolder;
+        import org.springframework.web.bind.annotation.*;
 
 
-import java.io.IOException;
-import java.security.Principal;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Map;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+        import java.io.IOException;
+        import java.security.Principal;
+        import java.util.*;
+        import javax.servlet.ServletException;
+        import javax.servlet.ServletRequest;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.validation.Valid;
 
 @RestController
 public class TransactionController {
@@ -63,7 +58,7 @@ public class TransactionController {
 //    }
 
 
-//    public ResponseEntity transferMoney(
+    //    public ResponseEntity transferMoney(
 //            @Parameter(in = ParameterIn.DEFAULT, description = "", required = true, schema = @Schema()) @Valid @RequestBody TransactionRequest body) {
 //        try {
 //            // require user to be authenticated
@@ -101,3 +96,4 @@ public class TransactionController {
         }
     }
 }
+
