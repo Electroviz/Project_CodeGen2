105463bfd8eba58a47f1c1864d2ad4922a19604b
Nicky Garcia Jorge <619307@student.inholland.nl>
2022-09-08 19:53:54 +0200

    tests

--- src/test/java/test/UserServiceTest.java
+++ src/test/java/test/UserServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
@@ -15,25 +17,63 @@ class UserServiceTest {
     UserService userService;
     @Test
     void addUserShouldNotReturnNull() {
-        //User user = userService.addUser()
-        //assertNotNull(user);
+        User user = new User();
+        userService.addUser(user, false);
+        Assertions.assertNotNull(user);
     }
 
     @Test
     void checkIfStringIsEmailWorks() {
-        UserService userService = new UserService();
+        userService = new UserService();
 
         Assertions.assertFalse(userService.checkIfStringIsEmail("test.com"));
     }
 
     @Test
     void checkIfUserInputIsWordReturnsFalseIfNotWord() {
-        UserService userService = new UserService();
+        userService = new UserService();
 
         Assertions.assertFalse(userService.checkIfUserInputIsWord("test123123"));
     }
 
     @Test
     void login() {
+        String token = userService.login("test", "test");
+        Assertions.assertNotNull(token);
+    }
+
+    @Test
+    void getUserIdByUsernameTest(){
+        User user = new User();
+        user.setUsername("Test");
+        try {
+            Long id = userService.getUserIdByUsername("Test");
+            Assertions.assertNotNull(id);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getUsersWithoutBankaccount(){
+        try {
+            List<User> userList = userService.getUsersWithoutBankAccount();
+            assertNotNull(userList);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getUserById(){
+        try {
+            User user = userService.getUserById(12L);
+            assertNotNull(user);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
